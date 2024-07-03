b9172b5c9ce1bc38f830c654d757eb44de0888fd
sgzh1 <ya.superkesa@gmail.com>
2024-07-04 00:32:05 +0300

    deleted tests

--- src/test/java/ru/stepanovgzh/mycoin/AppTest.java
+++ src/test/java/ru/stepanovgzh/mycoin/AppTest.java
@@ -1,20 +1,5 @@
 package ru.stepanovgzh.mycoin;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
 }
