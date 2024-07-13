c397bc296f3de7b540703bc9677102353792d433
sgzh1 <ya.superkesa@gmail.com>
2024-07-13 17:24:51 +0300

    added mining thread

--- src/main/java/ru/stepanovgzh/mycoin/threads/MiningThread.java
+++ src/main/java/ru/stepanovgzh/mycoin/threads/MiningThread.java
@@ -1,10 +1,56 @@
 package ru.stepanovgzh.mycoin.threads;
 
-public class MiningThread {
+import ru.stepanovgzh.mycoin.servicedata.BlockchainData;
 
-    public void start() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class MiningThread extends Thread
+{
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            long lastMinedBlock = LocalDateTime.parse(
+                    BlockchainData.getInstance().getCurrentBlockChain().getLast().getTimeStamp())
+                .toEpochSecond(ZoneOffset.UTC);
+            if (lastMinedBlock + BlockchainData.getTimeoutInterval()
+                < LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
+            {
+                System.out.println("BlockChain is too old for mining! Update it from peers");
+            }
+            else if (lastMinedBlock + BlockchainData.getMiningInterval()
+                - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) > 0)
+            {
+                System.out.println("BlockChain is current, mining will commence in "
+                    + (lastMinedBlock + BlockchainData.getMiningInterval()
+                        - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) + " seconds");
+            }
+            else
+            {
+                System.out.println("MINING NEW BLOCK");
+                BlockchainData.getInstance().mineBlock();
+                System.out.println(BlockchainData.getInstance().getWalletBallanceFX());
+            }
+            System.out.println(LocalDateTime.parse(
+                    BlockchainData.getInstance().getCurrentBlockChain().getLast().getTimeStamp())
+                .toEpochSecond(ZoneOffset.UTC));
+
+            try
+            {
+                Thread.sleep(2000);
+                if (BlockchainData.getInstance().isExit())
+                {
+                    break;
+                }
+                BlockchainData.getInstance().setMiningPoints(
+                    BlockchainData.getInstance().getMiningPoints() + 2);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
