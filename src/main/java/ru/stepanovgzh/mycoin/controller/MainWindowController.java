1c117507dd68fecc6ddfa90aac54c03d07277418
sgzh1 <ya.superkesa@gmail.com>
2024-07-13 13:17:49 +0300

    added controller for main window

---
+++ src/main/java/ru/stepanovgzh/mycoin/controller/MainWindowController.java
@@ -0,0 +1,33 @@
+package ru.stepanovgzh.mycoin.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import ru.stepanovgzh.mycoin.model.Transaction;
+
+public class MainWindowController 
+{
+    //this is read-only UI table
+    @FXML
+    public TableView<Transaction> tableView = new TableView<>();
+    @FXML
+    private TableColumn<Transaction, String> from;
+    @FXML
+    private TableColumn<Transaction, String> to;
+    @FXML
+    private TableColumn<Transaction, Integer> value;
+    @FXML
+    private TableColumn<Transaction, String> timeStamp;
+    @FXML
+    private TableColumn<Transaction, String> signature;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private TextField eCoins;
+    @FXML
+    private TextArea publicKey;
+    
+}
