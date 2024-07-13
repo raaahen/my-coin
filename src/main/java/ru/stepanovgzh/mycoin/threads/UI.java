daa30b3ed46e2b4c52eddfb9cdd4c5b8d79e5aca
sgzh1 <ya.superkesa@gmail.com>
2024-07-13 15:31:21 +0300

    added UI application thread

--- src/main/java/ru/stepanovgzh/mycoin/threads/UI.java
+++ src/main/java/ru/stepanovgzh/mycoin/threads/UI.java
@@ -1,12 +1,29 @@
 package ru.stepanovgzh.mycoin.threads;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class UI {
+import java.io.IOException;
 
-    public void start(Stage primaryStage) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+public class UI extends Application
+{
+    @Override
+    public void start(Stage stage)
+    {
+        Parent root = null;
+        try
+        {
+            root = FXMLLoader.load(getClass().getResource("src/main/fxml/view/MainWindow.fxml"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        stage.setTitle("My-Coin");
+        stage.setScene(new Scene(root, 900, 700));
+        stage.show();
     }
-    
 }
